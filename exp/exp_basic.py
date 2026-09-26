ccf6b324f08c7c6f4b2e0a81f81640e90781685c
wanghq21 <32741441+wanghq21@users.noreply.github.com>
2026-09-26 15:54:10 +0800

    update Exp_Basic

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -1,13 +1,13 @@
 import os
 import torch
-from models import FACT
+from models import TimeRelay
 
 
 class Exp_Basic(object):
     def __init__(self, args):
         self.args = args
         self.model_dict = {
-            'FACT':FACT,
+            'TimeRelay':TimeRelay,
         }
         if args.model == 'Mamba':
             print('Please make sure you have successfully installed mamba_ssm')
